46051f539b101fec6c78d7421e160de220c5a2e6
AikiToWeb <baekjongheon7418@gmail.com>
2022-06-21 23:22:51 +0900

    second commit

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ let userInput = document.getElementById("userInput");
 let resultArea = document.getElementById("resultArea"); 
 let answerBTN = document.getElementById("answerBTN");
 let answerArea = document.getElementById("answerArea");
+let resetBtn = document.getElementById("resetBtn"); 
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chanceArea");
+let history = [];
 
 playButton.addEventListener("click", play);
 answerBTN.addEventListener("click", showAnswer);
+resetBtn.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){userInput.value = ""});
 
+/* 랜덤번호 생성  */
 function pickRandomNum()
 {
     compterNum = Math.floor( Math.random() * 100) + 1;      
@@ -20,9 +28,27 @@ function pickRandomNum()
 
 }
 
+
 function play()
 {
     let userValue = userInput.value;
+    
+    if (userValue < 1 || userValue >100)
+    {
+        resultArea.textContent = "1과 100 사이의 숫자만 입력해주세요.";
+        return;
+    }
+
+    if (history.includes(userValue))
+    {
+        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.";
+        return;
+    }
+
+    chances--;
+    chanceArea.textContent = `남은 기회: ${chances}번`;
+
+
     if(userValue < compterNum)
     {
         resultArea.textContent = "UP!"
@@ -34,14 +60,37 @@ function play()
     else 
     {
         resultArea.textContent = "CORRECT!";
+        // playButton.disabled = true;
+        gameOver = true;
     }
 
-    console.log("userValue :" +  userValue);
+    history.push(userValue);    // 사용자가 입력한 값을 history안에 배열로 추가
+    console.log(history);
+
+
+    if (chances < 1)
+    {
+        gameOver = true;
+    }
+
+    if (gameOver == true)
+    {
+        playButton.disabled = true;
+    }
 }
 
 function showAnswer()
 {
     answerArea.textContent = compterNum;
+}
+
+function reset()
+{
+    userInput.value = "";
+    pickRandomNum();
+    resultArea.textContent = "결과가 나온다";
+    playButton.disabled = false;
+    
 }
 
 pickRandomNum();
